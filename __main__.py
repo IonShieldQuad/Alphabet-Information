75fb7a3a7379f9ec01536351953d0ee203ed8312
IonShieldQuad <33602963+IonShieldQuad@users.noreply.github.com>
2020-03-12 22:18:48 +0300

    Added default alphabets

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 
+import default_alphabets as da
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
@@ -8,20 +9,29 @@ kivy.require('1.11.1')
 Config.set('graphics', 'width', '600')
 Config.set('graphics', 'height', '400')
 
-FILE_PATH = "alphabet.txt"
-LIMIT = 256
+file_path = "alphabet.txt"
+limit = 256
+
+alphabet = None
 
 
 class InfoWidget(Widget):
     def read_file_button(self):
         try:
-            out = read_file(FILE_PATH)
+            out = read_file(file_path)
             print("File:\r\n")
             print(out)
             self.ids.out1.text = "{}".format(len(out))
         except FileNotFoundError:
-            print("File {} not found\r\n".format(FILE_PATH))
+            print("File {} not found\r\n".format(file_path))
         return
+
+    def display_alphabet_info(self, name):
+        out = self.ids.out1
+        if alphabet is None:
+            out.text = "No alphabet \r\nloaded"
+        else:
+            out.text = "Alphabet: \r\n {} \r\nCount: {}".format(name, len(alphabet))
 
 
 class InfoApp(App):
@@ -45,9 +55,8 @@ def read_file(uri):
 if __name__ == "__main__":
     try:
         print("File:\r\n")
-        print(read_file(FILE_PATH))
+        print(read_file(file_path))
     except FileNotFoundError:
-        print("File {} not found\r\n".format(FILE_PATH))
-
+        print("File {} not found\r\n".format(file_path))
 
     InfoApp().run()
